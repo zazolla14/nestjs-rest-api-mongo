918c88c55bfc4c5040de1cde086023f788be44e6
Azola Zubizarreta <za.ntik14@gmail.com>
2021-03-22 22:54:55 +0700

    connect to mongodb commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common'
+import { MongooseModule } from '@nestjs/mongoose'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { ItemsController } from './items/items.controller'
+import { ItemsModule } from './items/items.module'
 import { ItemsService } from './items/items.service'
 
 @Module({
-        imports: [],
+        imports: [ItemsModule, MongooseModule.forRoot()],
         controllers: [AppController, ItemsController],
         providers: [AppService, ItemsService],
 })
